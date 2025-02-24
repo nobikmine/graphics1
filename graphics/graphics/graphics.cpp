a9cfefa6a21662ec03370f5e024fa6ddce4890a4
Vadim Man'kov <144819624+nobikmine@users.noreply.github.com>
2025-02-24 03:27:10 +0300

    Add files via upload

---
+++ graphics/graphics/graphics.cpp
@@ -0,0 +1,53 @@
+﻿#include <glew.h>
+#include <glfw3.h>
+#include <iostream>
+
+void error_callback(int error, const char* description) {
+    std::cerr << "Error: " << description << std::endl;
+}
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwSetErrorCallback(error_callback);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Simple Example", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1.0f, 0.4f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glColor3f(0.7f, 0.1f, 0.6f);
+
+        glBegin(GL_QUADS);
+
+        glVertex2f(-0.5f, 0.5f);
+        glVertex2f(0.5f, 0.5f);
+
+        glVertex2f(0.7f, -0.5f);
+        glVertex2f(-0.7f, -0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
