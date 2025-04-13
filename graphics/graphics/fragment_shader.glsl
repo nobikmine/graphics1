fb1198a31c38ae67cbd571864d88d212e40eb5b1
Vadim Man'kov <144819624+nobikmine@users.noreply.github.com>
2025-04-13 10:27:11 +0300

    .

--- graphics/graphics/fragment_shader.glsl
+++ graphics/graphics/fragment_shader.glsl
@@ -5,7 +5,6 @@ uniform float timeValue;
 
 void main()
 {
-    // Используем синус времени для плавного изменения цвета
     float redValue = (sin(timeValue) + 1.0) / 2.0;
     float greenValue = (sin(timeValue + 2.0) + 1.0) / 2.0;
     float blueValue = (sin(timeValue + 4.0) + 1.0) / 2.0;
